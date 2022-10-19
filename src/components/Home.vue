d5b3ee5f7a24a2e4b73454c77feccdaf8f71bf2c
Angus Gaukroger <agaukroger77@gmail.com>
2022-10-19 19:23:23 +0100

    removed hello world and properly set up vuex.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,19 @@
 <template>
     <div >
-        <h1>Everything Installed Successfully
+        <h1 class="font-extrabold text-3xl">Everything Installed Successfully
          - GLHF</h1>
-        <p class="mt-5">Stack: Vue3+Vite, Tailwind CSS, Vue Router, Vue X, ESLint, Prettier</p>
+        <p class="my-5">Stack: Vue3+Vite, Tailwind CSS, Vue Router, VueX, ESLint, Prettier</p>
+        <p class="">Name in store: {{name}} ... This value is determined by VueX</p>
     </div>
 </template>
+
+<script setup>
+    import {computed} from 'vue';
+    import {useStore} from 'vuex';
+
+    const store = useStore();
+
+    const name = computed(() =>  {
+        return store.state.user.name;
+    })
+</script>
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,17 @@
 export default {
-    state: () => ({}),
+    state: () => ({
+        name: 'Memphis'
+    }),
 
     getters: {},
-    mutations: {},
-    actions: {},
+    mutations: {
+        SET_NAME(state, payload){
+            state.name = payload
+        }
+    },
+    actions: {
+        saveName({commit}, data) {
+            commit('SET_NAME', data)
+        }
+    },
 }
