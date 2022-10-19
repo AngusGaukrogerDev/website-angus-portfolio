5f5f76ce54fa38e7c3e5325b71dc204e5dddf555
Angus Gaukroger <agaukroger77@gmail.com>
2022-10-19 19:02:56 +0100

    Default Message

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        Everything Installed Successfully
-        GLHF
+    <div >
+        <h1>Everything Installed Successfully
+         - GLHF</h1>
+        <p class="mt-5">Stack: Vue3+Vite, Tailwind CSS, Vue Router, Vue X, ESLint, Prettier</p>
     </div>
 </template>
