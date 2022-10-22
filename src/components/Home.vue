d75099755ea74883f0d5a25067d834db16b4f396
Angus Gaukroger <agaukroger77@gmail.com>
2022-10-22 15:02:11 +0100

    added vueuse

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
     <div >
         <h1 class="aos-item font-extrabold text-3xl" data-aos="fade-left">Everything Installed Successfully
          - GLHF</h1>
-        <p class="my-5">Stack: Vue3+Vite, Tailwind CSS, Vue Router, VueX, Headless UI, AOS, ESLint, Prettier</p>
+        <p class="my-5">Stack: Vue3+Vite, Tailwind CSS, Vue Router, VueX, VueUse, Headless UI, AOS, ESLint, Prettier</p>
         <p class="">Name in store: {{name}} ... This value is determined by VueX</p>
     </div>
 </template>
